2fc42271f21e77cc6d1570696f04d01841b30d5d
Zac Gibson <zgibson@live.com>
2020-07-05 11:19:52 -0500

    move structs to better place

--- cmd/hookbuffer/hookhandler.go
+++ cmd/hookbuffer/hookhandler.go
@@ -4,20 +4,6 @@ import (
 	"strings"
 )
 
-// Body structure of a Discord webhook
-type Body struct {
-	Content string  `json:"content"`
-	Embeds  []Embed `json:"embeds"`
-}
-
-// Embed structure for use inside Body
-type Embed struct {
-	Description string `json:"description"`
-	Title       string `json:"title"`
-	Text        string `json:"text"`
-	Color       int    `json:"color"`
-}
-
 // HandleHook takes in the Hook from httpserver
 // parses out info, makes some decisions, sends to forward queue
 func HandleHook(h *Hook) {
