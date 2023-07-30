f61c7381798362cd3b0c621f4de29d48802793e4
cbackas <zgibson@live.com>
2023-07-30 13:04:40 -0500

    Breaking: Move to Rust. With no code yet.

---
+++ release.config.cjs
@@ -0,0 +1,61 @@
+/* eslint-disable indent */
+/* eslint-disable no-template-curly-in-string */
+/**
+ * @typedef {import('semantic-release').Options} Options
+ */
+
+const releaseRules = [
+  { tag: 'Breaking', release: 'major' },
+  { tag: 'Fix', release: 'patch' },
+  { tag: 'Update', release: 'minor' },
+  { tag: 'New', release: 'minor' },
+  { tag: 'Build', release: 'patch' },
+  { tag: 'Upgrade', release: 'patch' },
+  { tag: 'Chore', release: 'patch' }
+]
+
+const noteKeywords = releaseRules.map(rule => rule.tag)
+
+/** @type {Options} */
+module.exports = {
+  branches: ['master'],
+  tagFormat: '${version}',
+  repositoryUrl: 'https://github.com/cbackas/Hookbuffer',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'eslint',
+        releaseRules
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'eslint',
+        parserOpts: {
+          noteKeywords
+        }
+      }
+    ],
+    [
+      '@semantic-release/github',
+      {
+        failTitle: false,
+        failComment: false,
+        labels: false,
+        releasedLabels: false
+      }
+    ],
+    [
+        '@codedependant/semantic-release-docker',
+        {
+          dockerTags: ['{{version}}', 'latest'],
+          dockerFile: 'Dockerfile',
+          dockerRegistry: 'ghcr.io',
+          dockerProject: 'cbackas',
+          dockerImage: 'Hookbuffer'
+        }
+      ]
+  ]
+}
