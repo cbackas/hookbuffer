acea249d8051a5743a9bff07129731b8eb1d4fdd
cbackas <git@zac.gg>
2025-02-26 19:45:49 -0600

    move required headers to top of fetch function

--- src/lib.rs
+++ src/lib.rs
@@ -16,17 +16,21 @@ mod structs;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    match req.headers().get("User-Agent") {
+        Ok(Some(user_agent)) if user_agent.starts_with("Sonarr/") => {}
+        _ => return Response::error("Invalid User-Agent", 400),
+    };
+
+    match req.headers().get("X-Custom-PSK") {
+        Ok(Some(psk)) if psk == env.secret("SECRET_KEY")?.to_string() => {}
+        _ => return Response::error("Invalid X-Custom-PSK", 401),
+    }
+
     Router::new()
         .on_async(
             "/api/webhooks/:id/:token",
             async |req: Request, ctx: RouteContext<()>| {
-                match req.headers().get("User-Agent") {
-                    Ok(Some(user_agent)) if user_agent.starts_with("Sonarr/") => {}
-                    _ => return Response::error("Invalid User-Agent", 400),
-                };
                 let group_id = ctx.param("id").unwrap();
-
-                console_log!("Recieved webhook for group: {}", group_id);
 
                 let namespace = ctx.durable_object("HOOKBUFFER")?;
                 let stub = namespace.id_from_name(group_id)?.get_stub()?;
